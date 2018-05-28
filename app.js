b103ad096e55734e264b2950f4f5f46e244c7106
root <root@localhost.localdomain>
2018-05-28 21:42:27 +0100

    changes made to js file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
   //create the function
   function nextIte(profile) {
+    //set the index to 0
     let nextIndex = 0;
 
     return {
-      next: function() {
+      next() {
         return nextIndex < profile.length 
           ?{value: profile[nextIndex++], done: false}
           :
@@ -11,6 +12,12 @@
       }
     }
   }
+  let selector = {
+    next : 'next',
+    profile : 'profileDisplay',
+    image: 'imageDisplay'
+  }
+  
 //Using AJAX to fetch data
   var xhr = new XMLHttpRequest()
 
@@ -19,27 +26,29 @@
   xhr.onload = function() {
     if (this.status === 200) {
       let data = JSON.parse(this.responseText);
+      //set profile to the data coming from the API
       let profile = nextIte(data)
-      load()
+      //call the load function to load the first profile
+      loadProfile()
       //create an event listener for the button
-      document.getElementById('next').addEventListener('click', load)
+      document.getElementById(selector.next).addEventListener('click', loadProfile)
 
-      function load() {
+      function loadProfile() {
         //get the value of the current and next data
-        const current = profile.next().value
-          if(current !== undefined){
-            document.getElementById('profileDisplay').innerHTML =
+        const currentProfile = profile.next().value
+          //check if the current value is not undefined
+          if(currentProfile !== undefined){
+            document.getElementById(selector.profile).innerHTML =
+            `<ul class="list-group">
+                    <li class="list-group-item">Login: ${currentProfile.login}</li>
+                    <li class="list-group-item">Id: ${currentProfile.id}</li>
+                    <li class="list-group-item">Type: ${currentProfile.type}</li>
+              </ul>
             `
-                <ul class="list-group">
-                  
-                    <li class="list-group-item">Login: ${current.login}</li>
-                    <li class="list-group-item">Id: ${current.id}</li>
-                    <li class="list-group-item">Type: ${current.type}</li>
-                </ul>
-            `
-            document.getElementById('imageDisplay').innerHTML = `<img src="${current.avatar_url}" class="w-25">`;
+            document.getElementById(selector.image).innerHTML = `<img src="${currentProfile.avatar_url}" class="w-25">`;
           }
         else {
+          //reload the page after the last iteration
           window.location.reload()
         }
       }
